fd23d19d9c803b4de11fc716f65585deb48bfb90
Dylan Lott <lott.dylan@gmail.com>
2017-12-24 21:00:29 -0700

    adds update features

--- index.js
+++ index.js
@@ -4,14 +4,23 @@ const bodyParser = require('body-parser')
 
 const server = (config) => {
   const db = mongojs(config.db)
+
+  db.on('error', (err) => {
+    console.error('error connecting to database', err)
+  })
+
+  db.on('connect', () => {
+    console.log('connected to database')
+  })
+
   const lib = require('./lib')(db, config)
   const app = express()
 
   app.use(bodyParser.json())
 
   app.get('/', lib.get)
-  app.get('/:collection', lib.find)
-  app.get('/:collection/:id', lib.findById)
+  app.get('/:model', lib.find)
+  app.get('/:model/:id', lib.findById)
   app.post('/', lib.create)
   app.put('/', lib.update)
   app.delete('/', lib.delete)
