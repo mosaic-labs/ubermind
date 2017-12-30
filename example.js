507c3509ddfa0d559f363ee217c092a5bd1a26ac
Dylan Lott <lott.dylan@gmail.com>
2017-12-30 01:47:18 -0700

    adds support for auth system
    - per route configurable auth system
    - adds better querying support for limit and skip
    - only log database connection in development mode

--- example.js
+++ example.js
@@ -8,7 +8,12 @@ app.use('/ubermind', ubermind({
   timestamps: true,
   webhook: 'http://localhost:5000/', // where it will post events to
   socket: 'http://localhost:4000', // socket.io url
-  db: process.env.MONGO_URI || 'localhost:27017/ubermind' // specify the database connection string
+  db: process.env.MONGO_URI || 'localhost:27017/ubermind', // specify the database connection string
+  permissions: {
+    getRoot: function (req, res, next) {
+      return next()
+    }// pass a middleware function
+  }
 }));
 
 app.listen(3000, () => {
--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require('express')
 const mongojs = require('mongojs')
 const bodyParser = require('body-parser')
+const noop = (req, res, next) => next()
 
 const server = (config) => {
   const app = express()
-  const server = require('http').Server(app);
+  const server = require('http').Server(app)
   const Events = require('./lib/events')
   const db = mongojs(config.db)
   const events = new Events(config, server)
   const lib = require('./lib')(db, config, events)
   const utils = require('./lib/utils')
+  const auth = config.permissions
 
   db.on('error', (err) => {
     events.broadcast('db_error', err)
@@ -28,14 +30,14 @@ const server = (config) => {
   app.set('db', db)
   app.set('config', config)
 
-  app.get('/', lib.root)
-  app.post('/', lib.create)
-  app.put('/', lib.update)
-  app.delete('/', lib.delete)
-  app.get('/:model', lib.find)
-  app.get('/:model/:id', lib.findOne) // :id only takes ObjectID's
-  app.put('/:model/:id', lib.updateOne) // :id only takes ObjectID's
-  app.delete('/:model/:id', lib.deleteById) // :id only takes ObjectID's
+  app.get('/', auth.getRoot || noop, lib.root)
+  app.post('/', auth.create || noop, lib.create)
+  app.put('/', auth.update || noop, lib.update)
+  app.delete('/', auth.delete || noop, lib.delete)
+  app.get('/:model', auth.find || noop, lib.find)
+  app.get('/:model/:id', auth.findOne || noop, lib.findOne) // :id only takes ObjectID's
+  app.put('/:model/:id', auth.updateOne || noop, lib.updateOne) // :id only takes ObjectID's
+  app.delete('/:model/:id', auth.deleteOne || noop, lib.deleteById) // :id only takes ObjectID's
 
   return app
 }
